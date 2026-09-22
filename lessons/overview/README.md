fc14b24356663b140b904fff961aa743d69e6761
Chris Dunphy <chris.dunphy@fglsports.com>
2026-09-22 10:47:27 -0600

    [main] Exercise one is done

--- lessons/overview/README.md
+++ lessons/overview/README.md
@@ -4,10 +4,12 @@
 
 ## What is TypeScript
 
-- TypeScript is a programming language.
-- Compiles to JS.
-- Superset of JS.
-- Statically & Strongly typed.
+- TypeScript is a programming language
+  - Superset of JS
+    - JS code is valid TS code
+    - TS adds features (like the types)
+  - Compiles to JS
+  - Statically & strongly typed
 
 ### TypeScript Documentation
 
---
+++ src/overview/checkMe.ts
@@ -0,0 +1,24 @@
+let language = {
+  name: "JavaScript",
+  officialName: "ECMAScript",
+  released: 1995,
+  creator: "Brendan Eich",
+  company: "Netscape",
+}
+
+console.log(language)
+console.log(language.officialName)
+console.log(language.creator)
+
+language = {
+  name: "TypeScript",
+  officialName: "TypeScript",
+  released: 2012,
+  creator: "Anders Hejlsberg",
+  company: "Microsoft",
+}
+
+console.log(language)
+console.log(language.released)
+console.log(language.company)
+console.log(language.officialName)
